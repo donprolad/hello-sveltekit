a4e6b9ce586f77d6f4caf6ae9bf8323ee31272eb
Don <dghdp37@gmail.com>
2022-04-18 16:51:15 +0200

    added status to make sense of the air pollution number

---
+++ jest.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    transform: {
+        '^.+\\.svelte$':[
+            'svelte-jester',
+            {
+                preprocess: 'svelte.config.test.cjs'
+            }
+        ],
+        '^.+\\.ts$': 'ts-jest',
+        '^.+\\.js$': 'ts-jest'
+    },
+    moduleFileExtensions: ['js', 'ts', 'svelte'],
+    moduleNameMapper: {
+        '^\\$lib(.*)$': '<rootDir>/src/lib$1',
+        '^\\$tests(.*)$': '<rootDir>/src/tests$1',
+        '^\\$utilities(.*)$': '<rootDir>/src/utilities$1',
+        '^\\$app(.*)$': [
+           '<rootDir>/.svelte-kit/dev/runtime/app$1',
+           '<rootDir>/.svelte-kit/build/runtime/app$1',
+        ]
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
+}
--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -10,6 +10,24 @@
     Category: Web Pages" />
     <!-- <meta http-equiv="Content-Security-Policy" content="script-src 'self' http://localhost:3000/login; img-src 'self'" /> -->
 </svelte:head>
-<CurrentCity />
-<CountrySelector />
+<div class="head-container">
+    <div class="current-city">
+        <CurrentCity />
+    </div>
+    
+</div>
 
+<CountrySelector />
+<style>
+    .head-container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-areas:
+        ". current-city current-city current-city";
+    }
+
+    .current-city{
+        grid-area: current-city;
+        width: 100%;
+    }
+</style>
